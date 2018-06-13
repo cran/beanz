0898e5c5f56cd50e3f3c855100306405159728d6
Chenguang Wang <cwang68@jhmi.edu>
2018-06-13 20:38:01 +0000

    version 2.3

--- R/beanz_package.R
+++ R/beanz_package.R
@@ -180,4 +180,8 @@
 #'
 #' Dixon DO, Simon R (1991). Bayesian subset analysis. Biometrics, 47(3), 871-881.
 #'
+#' Wang C, Louis TA, Henderson NC, Weiss CO, Varadhan R (2018). beanz: An R
+#' Package for Bayesian Analysis of Heterogeneous Treatment Effects with a
+#' Graphical User Interface. Journal of Statistical Software, 85(7), 1-31.
+#'
 NULL
